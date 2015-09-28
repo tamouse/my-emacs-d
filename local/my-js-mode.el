b0ea07b5ffa72b9dcea1a8ca0536da4180d36687
Tamara Temple <tamouse@gmail.com>
2015-09-27 22:03:05 -0500

    Use js3-mode

--- local/my-js-mode.el
+++ local/my-js-mode.el
@@ -1,4 +1,4 @@
-(autoload 'js-mode "Javascript" "Start js-mode" t)
-(add-to-list 'auto-mode-alist '("\\.js" . js-mode))
-(add-to-list 'auto-mode-alist '("\\.json" . js-mode))
-(setq js-indent-level 2)
+(package-install-if-needed 'js3-mode)
+(autoload 'js3-mode "js3" "Start js3-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js" . js3-mode))
+(add-to-list 'auto-mode-alist '("\\.json" . js3-mode))
