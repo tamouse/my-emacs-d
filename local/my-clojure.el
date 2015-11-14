e6af767fdc27cfa7f964905d4ee25986f53f7b32
Tamara Temple <tamouse@gmail.com>
2015-11-13 18:57:35 -0600

    update my-clojure.el

--- local/my-clojure.el
+++ local/my-clojure.el
@@ -1,1 +1,14 @@
 ;;; Clojure Setup
+
+(mapc 'package-install-if-needed
+      '(cider
+	clojure-mode
+	paredit
+	clojure-snippets
+	rainbow-delimiters
+	))
+
+(require 'clojure-mode)
+(require 'cider)
+(require 'paredit)
+(require 'rainbow-delimiters)
